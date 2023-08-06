c94f8886e529733df1a48404f172de32a475bcc5
phuckhoa33 <phuckhoa81@gmail.com>
2023-08-07 00:57:39 +0700

    fix

--- apps/user-api/src/main.ts
+++ apps/user-api/src/main.ts
@@ -4,13 +4,18 @@ import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule
-  );
-  app.use(cookieParser());
-  app.enableCors();
-  app.setViewEngine('hbs');
-  const port: number = parseInt(`${process.env.PORT}`) || 8000;
-  await app.listen(port);
+  try {
+    const app = await NestFactory.create<NestExpressApplication>(
+      AppModule
+      );
+      app.use(cookieParser());
+      app.enableCors();
+      app.setViewEngine('hbs');
+      const port: number = parseInt(`${process.env.PORT}`) || 8000;
+      await app.listen(port);
+    
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+  }
 }
 bootstrap();
